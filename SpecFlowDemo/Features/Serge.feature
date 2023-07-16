9f25f77c887327e408300fdc3bc10ea2b2829976
sam-klok <sklok11@gmail.com>
2023-07-15 17:34:49 -0700

    Writing Gherkin feature for Serge's study

---
+++ SpecFlowDemo/Features/Serge.feature
@@ -0,0 +1,10 @@
+﻿Feature: Is Serge studied today
+	Calculate if Serge actually studied
+
+
+@tag1
+Scenario: Serge have something to study
+	Given Serge have computer
+	When Task arrive
+	Then Read study task 
+	And at least for 1 hour
---
+++ SpecFlowDemo/StepDefinitions/IsSergeStudiedTodayStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowDemo.StepDefinitions
+{
+    [Binding]
+    public class IsSergeStudiedTodayStepDefinitions
+    {
+        [Given(@"Serge have computer")]
+        public void GivenSergeHaveComputer()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Task arrive")]
+        public void WhenTaskArrive()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"Read study task")]
+        public void ThenReadStudyTask()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"at least for (.*) hour")]
+        public void ThenAtLeastForHour(int p0)
+        {
+            throw new PendingStepException();
+        }
+    }
+}
